36d581527a545573ef329265a2ab03d577c8d5a6
mumbi <mumbi099@gmail.com>
2023-07-10 16:33:06 +0300

    Done code challenge

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,27 @@
+require_relative 'article'
 class Author
-  attr_accessor :name
+    attr_accessor :name
 
+    def initialize(name)
+      @name = name
 
-  def initialize(name)
-    @name = name
-  
-  end
+    end
+
+    def articles
+      Article.all.select { |article| article.author == self }
+    end
+
+    def magazines
+      articles.map { |article| article.magazine }.uniq
+    end
+
+    def add_article(magazine, title)
+      Article.new(self, magazine, title)
+    end
+
+    def topic_areas
+      magazines.map { |magazine| magazine.category }.uniq
+    end
 
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,50 @@
+require_relative 'article'
 class Magazine
-  attr_accessor :name, :category
+    attr_accessor :name, :category
+    @@all = []
 
+    def initialize(name, category)
+      @name = name
+      @category = category
+      @@all << self
 
-  def initialize(name, category)
-    @name = name
-    @category = category
+    end
 
-  end
+    def self.all
+      @@all
+    end
 
+    def contributors
+      Article.all.select { |article| article.magazine == self }.map { |article| article.author }
+    end
+
+    def self.find_by_name(name)
+      @@all.find { |magazine| magazine.name == name }
+    end
+
+    def article_titles
+      Article.all.select { |article| article.magazine == self }.map { |article| article.title }
+    end
+
+    def contributing_authors
+      authors = contributors.uniq
+      contributing_authors = []
+    
+      authors.each do |author|
+        articles_count = 0
+    
+        contributors.each do |contributor|
+          if contributor == author
+            articles_count += 1
+          end
+        end
+    
+        if articles_count > 2
+          contributing_authors << author
+        end
+      end
+    
+      contributing_authors
+    end
 
 end
